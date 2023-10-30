fc763664d9f6b6c992458ad802e2fd7a0c2edee6
Salman Ghumsani <sourabh@appinlay.com>
2023-10-30 16:08:27 +0530

    logo added on home screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,15 +30,19 @@ function Header() {
     }, []);
 
     return (
-        <View >
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
             <TouchableOpacity onPress={handleProfile}>
                 <View style={{ backgroundColor: '#5F76FD', justifyContent: 'center', alignItems: 'center', height: 35, width: 35, borderRadius: 50, marginLeft: 10 }}>
-                {userData?.userData?.imageUrl  ?  <Image style={{ height: "100%", width: "100%",borderRadius:20 }} source={{uri:userData?.userData?.imageUrl}} /> :
-                <Image style={{ height: "100%", width: "100%",borderRadius:20 }} source={require("../assets/img/profileImg.png")} />
-                }
+                    {userData?.userData?.imageUrl ? <Image style={{ height: "100%", width: "100%", borderRadius: 20 }} source={{ uri: userData?.userData?.imageUrl }} /> :
+                        <Image style={{ height: "100%", width: "100%", borderRadius: 20 }} source={require("../assets/img/profileImg.png")} />
+                    }
                 </View>
             </TouchableOpacity>
 
+            <Image
+                source={require("../assets/img/welcomeimg.png")} // Use the correct source for the right image
+                style={{ height: 150, width: 150, borderRadius: 50, marginLeft: '25%' }}
+            />
         </View>
     );
 }
